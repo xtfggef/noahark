39b5f56150ae086a23fff01a5033b2506780e7cd
xtfggef <xtfggef@163.com>
2017-04-07 01:23:20 +0800

    udpate

--- server/app.js
+++ server/app.js
@@ -1,201 +1,87 @@
-// 引入模块
+/**
+ * 服务入口，通过rest api提供服务
+ *
+ * @author 二青
+ */
+
 var restify = require('restify');
-var mongojs = require("mongojs");
+var article = require('./lib/article');
+// var user = require('./lib/user');
+// var category = require('./lib/category');
+
 
 // 服务器定义
 var server = restify.createServer({
-    name : "noahark"
+    name: "noahark"
 });
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 server.use(restify.CORS());
 
-// 数据库连接信息
-var connection_string = '127.0.0.1:27017/noahark';
-var db = mongojs(connection_string, ['noahark']);
-var user = db.collection("user");
-var article = db.collection("article");
-var module = db.collection("module");
-var category = db.collection("category");
-var articleItem = db.collection("articleitem");
-
 // 首页数据
-var index_path = '/'
-server.get({path : index_path , version : '0.0.1'} , findIndexDataMock);
+// var index_path = '/'
+// server.get({path: index_path, version: '0.0.1'}, findIndexDataMock);
 
-// 类目页数据
-// var category_path = '/category'
-// server.get({path : PATH +'/:catId' , version : '0.0.1'} , findCategoryData);
+// 文章相关
+var article_path = '/article'
+server.post({path: article_path, version: '0.0.1'}, postArticleAction);
+server.get({path: article_path, version: '0.0.1'}, findAllAction);
+server.get({path: article_path + '/:id', version: '0.0.1'}, findByIdAction);
+server.del({path: article_path + '/:jobId', version: '0.0.1'}, deleteAction);
 
-// 首页数据处理
-function findIndexDataMock(req, res , next){
-    res.setHeader('Access-Control-Allow-Origin','*');
-    var data = {
-        "modules": [
-            {
-                "id": "mo01",
-                "name": "养生专区",
-                "category": [
-                    {
-                        "id": "cid01",
-                        "name": "膳食",
-                        "image": "http://qty83k.creatby.com/materials/origin/c9d02bea043b4223ebce53dff1f7e4ab_origin.png",
-                        "url": "../articlelist/articlelist"
-                    },
-                    {
-                        "id": "cid02",
-                        "name": "调息",
-                        "image": "http://qty83k.creatby.com/materials/origin/df80c40c531659e206df0ed0c1df9447_origin.png",
-                        "url": "../articlelist/articlelist"
-                    },
-                    {
-                        "id": "cid03",
-                        "name": "运动",
-                        "image": "http://qty83k.creatby.com/materials/origin/08338391421e09eb953abaa6fc2491d5_origin.png",
-                        "url": "../articlelist/articlelist"
-                    },
-                    {
-                        "id": "cid04",
-                        "name": "心理",
-                        "image": "http://qty83k.creatby.com/materials/origin/fda268308a114bbe1dd06efed0107b2d_origin.png",
-                        "url": "../articlelist/articlelist"
-                    },
-                    {
-                        "id": "cid05",
-                        "name": "太极",
-                        "image": "http://qty83k.creatby.com/materials/origin/aa5d7911f55886e3f57834326ae43cb7_origin.png",
-                        "url": "../articlelist/articlelist"
-                    }
-                ]
-            },
-            {
-                "id": "mo02",
-                "name": "特殊人群",
-                "category": [
-                    {
-                        "id": "cid01",
-                        "name": "老人",
-                        "image": "http://qty83k.creatby.com/materials/origin/8450abbdfc0884a819b0936c8cf9ad23_origin.png",
-                        "url": "../articlelist/articlelist"
-                    },
-                    {
-                        "id": "cid02",
-                        "name": "孕妇",
-                        "image": "http://qty83k.creatby.com/materials/origin/16c59c2be24e7d7a6c2ddd9bf49e5803_origin.png",
-                        "url": "../articlelist/articlelist"
-                    },
-                    {
-                        "id": "cid03",
-                        "name": "婴儿",
-                        "image": "http://qty83k.creatby.com/materials/origin/d20e5fce53df6abd3c5c4b1750768ca7_origin.png",
-                        "url": "../articlelist/articlelist"
-                    },
-                    {
-                        "id": "cid04",
-                        "name": "学生",
-                        "image": "http://qty83k.creatby.com/materials/origin/6402b967b9e92ae246a8c3ea424d4cbe_origin.png",
-                        "url": "../articlelist/articlelist"
-                    },
-                    {
-                        "id": "cid05",
-                        "name": "残疾人",
-                        "image": "http://qty83k.creatby.com/materials/origin/dc1d3326b41734a3cbe4114dc7abc492_origin.png",
-                        "url": "../articlelist/articlelist"
-                    }
-                ]
-            }
-        ]
-    };
-    res.send(200, data)
+/**
+ * 新增文章
+ */
+function postArticleAction(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var result = article.postNew(req.params)
+    res.send(200, result);
+    return next();
 }
 
-function findIndexData(req, res , next){
-    res.setHeader('Access-Control-Allow-Origin','*');
-    jobs.find().limit(20).sort({postedOn : -1} , function(err , success){
-        console.log('[findIndexData success] ' + success);
-        console.log('[Response error] ' + err);
-        if(success){
-            res.send(200 , success);
-            return next();
-        }else{
-            return next(err);
-        }
-    });
+/**
+ * 获取所有文章
+ */
+function findAllAction(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var result = article.findAll(req.params)
+
+        res.send(200, result);
+        return next();
 }
 
+/**
+ * 通过ID查询文章
+ */
+function findByIdAction(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var result = article.findById(req.params.id)
+    if (result) {
+        res.send(200, result);
+        return next();
+    } else {
+        return next(err);
+    }
+}
+
+/**
+ * 通过ID删除文章
+ */
+function deleteAction(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var result = article.deleteById(req.params.id)
+    if (result) {
+        res.send(200, result);
+        return next();
+    } else {
+        return next(err);
+    }
+}
 
 // 服务器信息设置
 var ip_addr = '127.0.0.1';
-var port    =  '8000';
+var port = '8000';
 
-server.listen(port ,ip_addr, function(){
-    console.log('%s listening at %s ', server.name , server.url);
+server.listen(port, ip_addr, function () {
+    console.log('%s listening at %s ', server.name, server.url);
 });
-
-// server.get({path : PATH +'/:jobId' , version : '0.0.1'} , findJob);
-// server.post({path : PATH , version: '0.0.1'} ,postNewJob);
-// server.del({path : PATH +'/:jobId' , version: '0.0.1'} ,deleteJob);
-//
-// function findAllJobs(req, res , next){
-//     res.setHeader('Access-Control-Allow-Origin','*');
-//     jobs.find().limit(20).sort({postedOn : -1} , function(err , success){
-//         console.log('Response success '+success);
-//         console.log('Response error '+err);
-//         if(success){
-//             res.send(200 , success);
-//             return next();
-//         }else{
-//             return next(err);
-//         }
-//     });
-//
-// }
-//
-// function findJob(req, res , next){
-//     res.setHeader('Access-Control-Allow-Origin','*');
-//     jobs.findOne({_id:mongojs.ObjectId(req.params.jobId)} , function(err , success){
-//         console.log('Response success '+success);
-//         console.log('Response error '+err);
-//         if(success){
-//             res.send(200 , success);
-//             return next();
-//         }
-//         return next(err);
-//     })
-// }
-//
-// function postNewJob(req , res , next){
-//     var job = {};
-//     job.title = req.params.title;
-//     job.description = req.params.description;
-//     job.location = req.params.location;
-//     job.postedOn = new Date();
-//
-//     res.setHeader('Access-Control-Allow-Origin','*');
-//
-//     jobs.save(job , function(err , success){
-//         console.log('Response success '+success);
-//         console.log('Response error '+err);
-//         if(success){
-//             res.send(201 , job);
-//             return next();
-//         }else{
-//             return next(err);
-//         }
-//     });
-// }
-//
-// function deleteJob(req , res , next){
-//     res.setHeader('Access-Control-Allow-Origin','*');
-//     jobs.remove({_id:mongojs.ObjectId(req.params.jobId)} , function(err , success){
-//         console.log('Response success '+success);
-//         console.log('Response error '+err);
-//         if(success){
-//             res.send(204);
-//             return next();
-//         } else{
-//             return next(err);
-//         }
-//     })
-//
-// }
--- server/lib/article.js
+++ server/lib/article.js
@@ -1,67 +1,76 @@
-var findAllJobs = function findAllJobs(req, res , next){
-    res.setHeader('Access-Control-Allow-Origin','*');
-    jobs.find().limit(20).sort({postedOn : -1} , function(err , success){
-        console.log('Response success '+success);
-        console.log('Response error '+err);
-        if(success){
-            res.send(200 , success);
-            return next();
-        }else{
-            return next(err);
-        }
+/**
+ * 文章读写模块
+ *
+ * @author 二青
+ * @type {*}
+ */
+var mongojs = require("mongojs");
+// var mongojs = require('promised-mongo');
+var connection_string = '127.0.0.1:27017/noahark';
+var db = mongojs(connection_string, ['noahark']);
+var collect = db.collection("article");
+
+/**
+ * 创建新文章
+ *
+ * @param params
+ */
+var postNew = function postNew(params) {
+    var article = {};
+    article.title = params.title;
+    article.description = params.description;
+    article.location = params.location;
+    article.postedOn = new Date();
+    collect.save(article, function (err, result) {
+        console.log('[article-postNew] result: ' + result.toString());
+        console.log('[article-postNew] error: ' + err.toString());
+        return result;
+    });
+}
+
+/**
+ * 分页查询所有文章
+ *
+ * @param req
+ * @param res
+ */
+var findAll = function findAll(params) {
+    collect.find().limit(20).sort({postedOn: -1}, function (err, result) {
+        console.log('[article-findAll] result: ' + result.toString());
+        console.log('[article-findAll] error: ' + err.toString());
+        return result;
     });
 
 }
 
-var findJob = function findJob(req, res , next){
-    res.setHeader('Access-Control-Allow-Origin','*');
-    jobs.findOne({_id:mongojs.ObjectId(req.params.jobId)} , function(err , success){
-        console.log('Response success '+success);
-        console.log('Response error '+err);
-        if(success){
-            res.send(200 , success);
-            return next();
-        }
-        return next(err);
+/**
+ * 通过ID查找文章
+ *
+ * @param id
+ */
+var findById = function findById(id) {
+    collect.findOne({_id: mongojs.ObjectId(id)}, function (err, result) {
+        console.log('[article-findById] result: ' + result.toString());
+        console.log('[article-findById] error: ' + err.toString());
+        return result;
     })
 }
 
-var postNewJob = function postNewJob(req , res , next){
-    var job = {};
-    job.title = req.params.title;
-    job.description = req.params.description;
-    job.location = req.params.location;
-    job.postedOn = new Date();
-
-    res.setHeader('Access-Control-Allow-Origin','*');
-
-    jobs.save(job , function(err , success){
-        console.log('Response success '+success);
-        console.log('Response error '+err);
-        if(success){
-            res.send(201 , job);
-            return next();
-        }else{
-            return next(err);
-        }
-    });
-}
-
-var deleteJob = function c(req , res , next){
-    res.setHeader('Access-Control-Allow-Origin','*');
-    jobs.remove({_id:mongojs.ObjectId(req.params.jobId)} , function(err , success){
-        console.log('Response success '+success);
-        console.log('Response error '+err);
-        if(success){
-            res.send(204);
-            return next();
-        } else{
-            return next(err);
-        }
+/**
+ * 删除文章
+ *
+ * @param req
+ * @param res
+ */
+var deleteById = function deleteById(id){
+    jobs.remove({_id:mongojs.ObjectId(id)} , function(err , result){
+        console.log('[article-deleteById] result: ' + result.toString());
+        console.log('[article-deleteById] error: ' + err.toString());
+        return result;
     })
 }
 
-exports.findAllJobs = findAllJobs;
-exports.findJob = findJob;
-exports.postNewJob = postNewJob;
-exports.deleteJob = deleteJob;
+exports.findAll = findAll;
+exports.findById = findById;
+exports.postNew = postNew;
+exports.deleteById = deleteById;
